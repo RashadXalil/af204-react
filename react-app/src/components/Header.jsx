f719e3f49835699b339817e734078e45fcb6f072
RashadXalil <rashadmkh@code.edu.az>
2023-11-17 15:18:13 +0400

    useEffect

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,6 +1,25 @@
+import React from "react"
+import "./Header.css"
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className="wrapper">
+            <div className="header" style={{ display: "flex", alignItems: "center" }}>
+                <div className="header__left">
+                    <h2>Salam</h2>
+                </div>
+                <div className="header__right">'
+
+                    <ul style={{ display: "flex" }}>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Shop</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="main">salam</div>
+        </div>
+
     )
 }
 export default Header
